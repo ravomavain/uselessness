3c7bcdbaa634486d17de3ca93f798864ca504453
Ravomavain <ravomavain@gmail.com>
2012-05-20 22:12:11 +0200

    Switch to libxml2, libroxml had trouble with names starting with '0'… @heinrich5991: here's why I _hate_ xml ;-)

--- urifs.c
+++ urifs.c
@@ -17,14 +17,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *
- * Requirement: libfuse, libcurl and libroxml (https://code.google.com/p/libroxml/)
+ * Requirement: libfuse, libcurl and libxml2
  *
- * Compile with: gcc -o urifs urifs.c -Wall -ansi -W -std=c99 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -lfuse -lcurl -lroxml -O3 -Wno-unused-parameter
+ * Compile with: gcc -O3 -o urifs urifs.c -Wall -ansi -W -std=c99 -D_GNU_SOURCE -Wno-unused-parameter `pkg-config --cflags --libs libxml-2.0 fuse libcurl`
  *
  */
 
 #define FUSE_USE_VERSION 26
- 
+
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
@@ -33,7 +33,11 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <curl/curl.h>
-#include <roxml.h>
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
 
 #define MAX_ENTRIES	512
 #define LOG_FILE	"/var/log/urifs.log"
@@ -114,24 +118,30 @@ static int urifs_getattr(const char *path, struct stat *stbuf)
 {
 	DEBUG("here")
 	int nb;
-	node_t *n = NULL;
-	node_t *a = NULL;
-	node_t *root = fuse_get_context()->private_data;
-	char *xpath = xpath_from_path(path);
+	xmlChar *value;
+	xmlNodeSetPtr nodes;
+	xmlNodePtr node;
+	xmlXPathObjectPtr xpathObj;
 
+	char *xpath = xpath_from_path(path);
 	if (!xpath)
 		return -ENOENT;
 
-	DEBUG("trying '%s'",path);
-	node_t **ans = roxml_xpath(root, xpath, &nb);
-	DEBUG("xpath: %s  -> %d nodes found",xpath,nb);
+	DEBUG("xpath: %s", xpath);
+
+	xpathObj = xmlXPathEvalExpression(xpath, fuse_get_context()->private_data);
 	xfree(xpath);
+	if(xpathObj == NULL)
+		return -ENOENT;
 
-	if(ans) {
-		n = ans[0];
+	nodes = xpathObj->nodesetval;
 
-		char *type = roxml_get_name(n, NULL, 0);
-		if(strcmp(type, "dir")==0 || strcmp(type, "root")==0)
+	if (nodes && nodes->nodeNr > 0)
+	{
+		DEBUG("%d nodes found", nodes->nodeNr)
+		node = nodes->nodeTab[0];
+
+		if(strcmp(node->name, "dir")==0 || strcmp(node->name, "root")==0)
 		{
 			DEBUG("dir")
 			stbuf->st_mode = S_IFDIR | S_IXUSR | S_IXGRP | S_IXOTH;
@@ -141,7 +151,6 @@ static int urifs_getattr(const char *path, struct stat *stbuf)
 			DEBUG("file")
 			stbuf->st_mode = S_IFREG;
 		}
-		roxml_release(type);
 
 		stbuf->st_ino = 0;
 		stbuf->st_mode |= S_IRUSR | S_IRGRP | S_IROTH /*| S_IWUSR | S_IWGRP | S_IWOTH*/;
@@ -156,159 +165,197 @@ static int urifs_getattr(const char *path, struct stat *stbuf)
 		stbuf->st_mtime = 0;
 		stbuf->st_ctime = 0;
 
-		if( (a = roxml_get_attr(n, "size", 0)) != NULL )
+		value = xmlGetProp(node, "size");
+		if(value)
 		{
-			char * value = roxml_get_content(a, NULL, 0, NULL);
 			stbuf->st_blocks = stbuf->st_size = atoll(value);
-			roxml_release(value);
+			xmlFree(value);
 		}
-		if( (a = roxml_get_attr(n, "uid", 0)) != NULL )
+
+		value = xmlGetProp(node, "uid");
+		if(value)
 		{
-			char * value = roxml_get_content(a, NULL, 0, NULL);
+			stbuf->st_blocks = stbuf->st_size = atoll(value);
+			xmlFree(value);
+		}
+
+		value = xmlGetProp(node, "gid");
+		if(value)
+		{
 			stbuf->st_uid = atoi(value);
-			roxml_release(value);
+			xmlFree(value);
 		}
-		if( (a = roxml_get_attr(n, "gid", 0)) != NULL )
+
+		value = xmlGetProp(node, "mode");
+		if(value)
 		{
-			char * value = roxml_get_content(a, NULL, 0, NULL);
-			stbuf->st_uid = atoi(value);
-			roxml_release(value);
-		}
-		if( (a = roxml_get_attr(n, "mode", 0)) != NULL )
-		{
-			char * value = roxml_get_content(a, NULL, 0, NULL);
 			stbuf->st_mode &= S_IFMT;
 			stbuf->st_mode |= strtol (value, NULL, 8);
-			roxml_release(value);
+			xmlFree(value);
 		}
-		if( (a = roxml_get_attr(n, "ctime", 0)) != NULL )
+
+		value = xmlGetProp(node, "ctime");
+		if(value)
 		{
-			char * value = roxml_get_content(a, NULL, 0, NULL);
 			stbuf->st_ctime = atoi(value);
-			roxml_release(value);
+			xmlFree(value);
 		}
-		if( (a = roxml_get_attr(n, "atime", 0)) != NULL )
+
+		value = xmlGetProp(node, "atime");
+		if(value)
 		{
-			char * value = roxml_get_content(a, NULL, 0, NULL);
 			stbuf->st_atime = atoi(value);
-			roxml_release(value);
+			xmlFree(value);
 		}
-		if( (a = roxml_get_attr(n, "mtime", 0)) != NULL )
+
+		value = xmlGetProp(node, "mtime");
+		if(value)
 		{
-			char * value = roxml_get_content(a, NULL, 0, NULL);
 			stbuf->st_mtime = atoi(value);
-			roxml_release(value);
+			xmlFree(value);
 		}
 		DEBUG("mode: %d", stbuf->st_mode)
 
-		roxml_release(ans);
+		xmlXPathFreeObject(xpathObj);
 		return 0;
 	}
+	else
+		DEBUG("Invalid Xpath")
+
+	xmlXPathFreeObject(xpathObj);
+
 	return -ENOENT;
 }
- 
+
 static int urifs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
 	DEBUG("here")
 	DEBUG("trying '%s'",path);
 	int i;
 	int nb;
-	node_t *n = NULL;
-	node_t *root = fuse_get_context()->private_data;
-	char *xpath = xpath_from_path(path);
-	if (!xpath)
-		return -ENOENT;
-	node_t **ans = roxml_xpath(root, xpath, &nb);
-	DEBUG("xpath: %s  -> %d nodes found",xpath,nb);
-	xfree(xpath);
+	char *xpath;
+	xmlChar *value;
+	xmlNodeSetPtr nodes;
+	xmlNodePtr node;
+	xmlXPathObjectPtr xpathObj;
 
-	if(ans)	{
-		n = ans[0];
-		roxml_release(ans);
+	char *tmp = xpath_from_path(path);
+	if (!tmp)
+		return -ENOENT;
+
+	if (asprintf(&xpath, "%s/*[@name]", tmp) == -1)
+	{
+		xfree(tmp);
+		return -ENOENT;
+	}
+	xfree(tmp);
+
+	DEBUG("xpath: %s", xpath);
+
+	xpathObj = xmlXPathEvalExpression(xpath, fuse_get_context()->private_data);
+	xfree(xpath);
+	if(xpathObj == NULL)
+		return -ENOENT;
+
+	nodes = xpathObj->nodesetval;
+
+	if (nodes)
+	{
+		nb = nodes->nodeNr;
+		DEBUG("%d nodes found", nb)
 
 		filler(buf, ".", NULL, 0);
 		filler(buf, "..", NULL, 0);
-
-		nb = roxml_get_chld_nb(n);
-		DEBUG("%d dirs", nb)
-		for(i = 0; i < nb; i++)	{
-			node_t *tmp = roxml_get_chld(n, NULL, i);
-			tmp = roxml_get_attr(tmp, "name", 0);
-			if(!tmp)
-				continue;
-			char *name = roxml_get_content(tmp, NULL, 0, NULL);
-			filler(buf, name, NULL, 0);
-			roxml_release(name);
+		for(i=0;i<nb;i++)
+		{
+			value = xmlGetProp(nodes->nodeTab[i], "name");
+			if(value)
+			{
+				filler(buf, value, NULL, 0);
+				xmlFree(value);
+			}
 		}
+		xmlXPathFreeObject(xpathObj);
 		return 0;
 	}
+	else
+		DEBUG("Invalid Xpath")
+
+	xmlXPathFreeObject(xpathObj);
+
 	return -ENOENT;
 }
- 
+
 static int urifs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
 {
 	DEBUG("here %s", path)
 	return -ENOENT;
 }
- 
+
 static int urifs_open(const char *path, struct fuse_file_info *fi)
 {
 	DEBUG("here")
 	int i = 0;
-	int nb;
 	uri_fd *fd = NULL;
-	node_t *a;
-	char *value;
-	node_t *root = fuse_get_context()->private_data;
+	xmlChar *value;
+	xmlNodeSetPtr nodes;
+	xmlNodePtr node;
+	xmlXPathObjectPtr xpathObj;
+
 	char *xpath = xpath_from_path(path);
 	if (!xpath)
 		return -ENOENT;
-	node_t **ans = roxml_xpath(root, xpath, &nb);
-	DEBUG("xpath: %s  -> %d nodes found",xpath,nb);
+
+	DEBUG("xpath: %s", xpath);
+
+	xpathObj = xmlXPathEvalExpression(xpath, fuse_get_context()->private_data);
 	xfree(xpath);
-
-	if(ans)	{
-		node_t *n = ans[0];
-		roxml_release(ans);
-		fd = (uri_fd*)malloc(sizeof(uri_fd));
-		if(!fd)
-			return -ENOENT;
-		if( (a = roxml_get_attr(n, "size", 0)) == NULL)
-		{
-			xfree(fd);
-			return -ENOENT;
-		}
-
-		value = roxml_get_content(a, NULL, 0, NULL);
-		fd->size = atoll(value);
-		roxml_release(value);
-
-		if( (a = roxml_get_attr(n, "uri", 0)) == NULL)
-		{
-			xfree(fd);
-			return -ENOENT;
-		}
-
-		value = roxml_get_content(a, NULL, 0, NULL);
-		fd->uri = strdup(value);
-		roxml_release(value);
-		if(!fd->uri)
-		{
-			xfree(fd);
-			return -ENOENT;
-		}
-
-		while((i < MAX_ENTRIES)&&(opened_files[i]))	{ i++; } 
-		if(i < MAX_ENTRIES)	{
-			fi->fh = i;
-			opened_files[i] = fd;
-
-			return 0;
-		}
-		xfree(fd->uri);
-		xfree(fd);
+	if(xpathObj == NULL)
 		return -ENOENT;
+
+	nodes = xpathObj->nodesetval;
+
+	if (nodes && nodes->nodeNr > 0)
+	{
+		DEBUG("%d nodes found", nodes->nodeNr)
+		node = nodes->nodeTab[0];
+
+		fd = (uri_fd*)malloc(sizeof(uri_fd));
+		if (fd)
+		{
+			value = xmlGetProp(node, "size");
+			if(value)
+			{
+				fd->size = atoll(value);
+				xmlFree(value);
+				value = xmlGetProp(node, "uri");
+				if(value)
+				{
+					fd->uri = strdup(value);
+					xmlFree(value);
+					if(fd->uri)
+					{
+						while((i < MAX_ENTRIES)&&(opened_files[i]))
+							i++;
+						if(i < MAX_ENTRIES)
+						{
+							fi->fh = i;
+							opened_files[i] = fd;
+							xmlXPathFreeObject(xpathObj);
+							return 0;
+						}
+						xfree(fd->uri);
+					}
+				}
+			}
+			xfree(fd);
+		}
 	}
+	else
+		DEBUG("0 node found")
+
+	xmlXPathFreeObject(xpathObj);
+
 	return -ENOENT;
 }
 
@@ -326,7 +373,7 @@ static int urifs_read(const char *path, char *buf, size_t size, off_t offset, st
 	fd = opened_files[fi->fh];
 
 	if (!fd) { return -ENOENT; }
-	
+
 	if((size + offset) >= fd->size)	{
 		bytes = fd->size - offset;
 	} else {
@@ -381,8 +428,8 @@ static int urifs_release(const char *path, struct fuse_file_info *fi)
 void urifs_cleanup(void *data)
 {
 	DEBUG("here")
-	DEBUG("freeing file table")
-	node_t *root = fuse_get_context()->private_data;
+	xmlXPathContextPtr xpathCtx = fuse_get_context()->private_data;
+	xmlDocPtr doc = xpathCtx->doc;
 	int i;
 	for(i=0; i<MAX_ENTRIES; i++)
 	{
@@ -394,7 +441,10 @@ void urifs_cleanup(void *data)
 	}
 	xfree(opened_files);
 	curl_global_cleanup();
-	roxml_close(root);
+
+	xmlXPathFreeContext(xpathCtx);
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
 }
 
 int urifs_flush(const char *path, struct fuse_file_info *fi)
@@ -450,18 +500,31 @@ int urifs_mkdir(const char *dir, mode_t ignored)
 void *urifs_init(struct fuse_conn_info *conn)
 {
 	DEBUG("here")
-	node_t *n = NULL;
-
-	opened_files = (uri_fd**)malloc(sizeof(uri_fd*)*MAX_ENTRIES);
+	xmlDocPtr doc;
+	xmlXPathContextPtr xpathCtx;
 
 	DEBUG("mounting %s",source_xml)
 
-	n = roxml_load_doc(source_xml);
-	DEBUG("loaded doc to %p mount init ok", n);
+	xmlInitParser();
+	LIBXML_TEST_VERSION
+
+	doc = xmlParseFile(source_xml);
+
+	if (doc == NULL)
+		exit(1);
+
+	xpathCtx = xmlXPathNewContext(doc);
+	if (xpathCtx == NULL)
+	{
+		xmlFreeDoc(doc);
+		exit(1);
+	}
+
+	opened_files = (uri_fd**)malloc(sizeof(uri_fd*)*MAX_ENTRIES);
 
 	curl_global_init(CURL_GLOBAL_ALL);
 
-	return n;
+	return xpathCtx;
 }
 
 static struct fuse_operations urifs_oper = {
@@ -504,7 +567,7 @@ static int urifs_opt_proc(void *data, const char *arg, int key, struct fuse_args
 			if(num == 1)	{
 				source_xml = strdup(arg);
 				return 0;
-			}	
+			}
 			break;
 	}
 	return 1;
